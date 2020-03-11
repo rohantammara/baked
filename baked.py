eddd2837a39551676f5d88e86286451ebfa524b2
rohan-o-tammara <ta.rohan550@gmail.com>
2020-03-11 20:55:13 +0530

    added baked pie

---
+++ baked.py
@@ -0,0 +1,43 @@
+"""
+Helpful bytes of code
+
+Author: Rohan Tammara
+"""
+
+#===============================================================#
+import time
+
+# Pair of methods to time a block of code inspired by MATLAB
+def tic(id="default_id"):
+    print("<Timer {0} started>".format(id))
+    return (id, time.time())
+
+def toc(start):
+    duration = time.time() - start[1]
+    print("<Timer {0} Stopped> Duration : {1}".format(start[0], duration))
+    return duration
+
+#===============================================================#
+
+# Faster alternative to morph.flatten for flattening dictionaries
+def dict_flatten(input_dict):
+    flat = {}
+    continue_flag = False
+    for key in input_dict:
+        if isinstance(input_dict[key], dict):
+            for k in input_dict[key]:
+                if isinstance(input_dict[key][k], dict) or isinstance(input_dict[key][k], list):
+                    continue_flag = True
+                flat[str(key)+'.'+str(k)] = input_dict[key][k]
+        elif isinstance(input_dict[key], list):
+            for i in range(len(input_dict[key])):
+                flat[str(key)+'[{}]'.format(i)] = input_dict[key][i]
+        else:
+            flat[key] = input_dict[key]
+    
+    if continue_flag:
+        flat = dict_flatten(flat)
+    
+    return flat
+
+#===============================================================#
